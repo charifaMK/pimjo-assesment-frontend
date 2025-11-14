10b25faa0accce1570d8d48fe9e80dfdb5b055e3
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-14 19:27:09 +0600

    refactor: Create barrel file for overview/dashboard components
    Adds an index file to centralize and simplify imports for utility components used in data displays, including LoadingSpinner, StatsCards, SearchAndFilter, TableHeader, UserRow, and PaginationControls

---
+++ components/overview/index.ts
@@ -0,0 +1,6 @@
+export { LoadingSpinner } from "./LoadingSpinner";
+export { StatsCards } from "./stats-cards";
+export { SearchAndFilter } from "./search-and-filter";
+export { TableHeader } from "./table-header";
+export { UserRow } from "./user-row";
+export { PaginationControls } from "./pagination-controls";
