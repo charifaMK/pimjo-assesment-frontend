8cd1c08e25d76c21bc5b5c37abd2a643f26c6a67
Md Khalid Hossen <mdkhalidhossen@hotmail.com>
2025-11-14 19:11:00 +0600

    refactor: Create barrel file for confirmation dialog components
    Adds an index file to simplify imports for all components used in the generic ConfirmationDialog and the specialized DeleteConfirmationDialog

---
+++ components/confirmation-dialog/index.ts
@@ -0,0 +1,6 @@
+export { ConfirmationDialog } from './confirmation-dialog';
+export { DeleteConfirmationDialog } from './delete-confirmation-dialog';
+export { DialogHeader } from './dialog-header';
+export { DialogIcon } from './dialog-icon';
+export { DialogContent } from './dialog-content';
+export { DialogActions } from './dialog-actions';
